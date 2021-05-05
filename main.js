d680112c4ad74f2faf6aa8bb50322abae2df520b
SathvikHegde <62826093+SathvikHegde@users.noreply.github.com>
2021-05-05 11:22:28 +0530

    dumb mistake

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ client.once('ready', () => {
 client.on('message', message =>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
  
-    const args = message.content.slice(prefix.length).split();
+    const args = message.content.slice(prefix.length);
     const command = args.shift().toLowerCase();
     
     console.log(command);
