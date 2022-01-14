b622a61e7134861a746371c2777241566ba8448d
SathvikHegde <62826093+SathvikHegde@users.noreply.github.com>
2022-01-14 22:33:16 +0530

    Update guildMemberAdd.js

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,8 +1,9 @@
 module.exports = (member, Discord, client) => {
-  console.log(`Member joined with name ${member.displayName}`);
+  console.log();
+  client.channels.cache.get("875013057208786984").send(`Member joined with name ${member.displayName}`);
   
   if(member.displayName.includes("RTFKT")) {
     member.ban();
-    console.log(`Banned ${member.displayName}`);
+    client.channels.cache.get("875013057208786984").send(`Banned ${member.displayName}`);
   }
 }
