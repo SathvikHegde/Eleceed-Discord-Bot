4101ef658d71225fba864690c776e3207e54adf2
SathvikHegdee <sathushirnika@gmail.com>
2022-01-14 13:39:03 +0530

    auto ban scammers

---
+++ events/guild/guildMemberAdd.js
@@ -0,0 +1,8 @@
+module.exports = (member) => {
+  console.log(`Member joined with name ${member.displayName}`);
+  
+  if(member.displayName.includes("RTFKT")) {
+    member.ban();
+    console.log(`Banned ${member.displayName}`);
+  }
+}
