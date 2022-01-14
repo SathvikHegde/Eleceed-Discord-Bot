bdde33fa436f3830569613beaa3e810d5e581400
SathvikHegdee <sathushirnika@gmail.com>
2022-01-14 22:41:04 +0530

    auto ban scammers

--- events/guild/guildMemberAdd.js
+++
@@ -1,9 +0,0 @@
-module.exports = (member, Discord, client) => {
-  console.log();
-  client.channels.cache.get("875013057208786984").send(`Member joined with name ${member.displayName}`);
-  
-  if(member.displayName.includes("RTFKT")) {
-    member.ban();
-    client.channels.cache.get("875013057208786984").send(`Banned ${member.displayName}`);
-  }
-}
--- main.js
+++ main.js
@@ -13,6 +13,15 @@ client.events = new Discord.Collection();
     useUnifiedTopology: true
 });*/
 
+client.on("guildMemberAdd", member => {
+  client.channels.cache.get("875013057208786984").send(`Member joined with name ${member.displayName}`);
+  
+  if(member.displayName.includes("RTFKT")) {
+    member.ban();
+    client.channels.cache.get("875013057208786984").send(`Banned ${member.displayName}`);
+  }
+});
+
 ['command_handler', 'event_handler'].forEach(handler =>{
     require(`./handlers/${handler}`)(client, Discord);
 })
